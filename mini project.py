659926a75c35597fdc65d02939c1b57c351c332d
kurasandeep <148242703+kurasandeep@users.noreply.github.com>
2024-03-15 11:43:46 +0530

    Add files via upload

---
+++ mini project.py
@@ -0,0 +1,13 @@
+num1=int(input("enter a number:"))
+num2=int(input("enter a number:"))
+num3=int(input("enter a number:"))
+num4=int(input("enter a number:"))
+count=1
+while count<10:
+  product1=num1*count
+  product2=num2*count
+  product3=num3*count
+  product4=num4*count
+  print(num1,"*",count,"=",product1,num2,"*",count,"=",product2,num3,"*",count,"=",product3,num4,"*",count,"=",product4)
+  count=count+1
+
