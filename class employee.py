081691de26b26602a8a7930555cd29b2c5cbcf09
kurasandeep <148242703+kurasandeep@users.noreply.github.com>
2024-03-15 11:42:51 +0530

    Add files via upload

---
+++ class employee.py
@@ -0,0 +1,10 @@
+class employee:
+#hidden member of the class
+      __password= '8888'
+      _id = '1245'
+      def details(self):
+        print("ID",(self._id))
+         #print("passowed:",(self__passowed)+"/n")
+hidden = Employee()
+hidden.Details()
+#print(hidden._Employee_password)
